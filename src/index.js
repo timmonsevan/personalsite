5f6e7dd4a813432220d7f7a391d92ce7f8005e8e
Evan <timmonsevan@gmail.com>
2026-06-09 06:26:26 -0400

    added morgan logging

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import http from "http";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -7,16 +8,19 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(
+  morgan("tiny", { stream: { write: (msg) => console.log(msg.trim()) } }),
+);
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  console.log(`GET ${__dirname} ../index.html`);
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/resume", (req, res) => {
-  console.log(`GET ${__dirname} ../resume/index.html`);
-  res.sendFile(path.join(__dirname, "../resume/index.html"));
+  res.sendFile(path.join(__dirname, "resume/index.html"));
 });
 
-http.createServer(app).listen(port, () => {
+http.createServer(app).listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
