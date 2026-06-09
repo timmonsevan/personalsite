0ba67629abfadd386765bb510532e06c9d4c5a84
Evan <timmonsevan@gmail.com>
2026-06-09 07:35:56 -0400

    added Doom Bandname Generator

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ import morgan from "morgan";
 import http from "http";
 import path from "path";
 import { fileURLToPath } from "url";
+import names from "./data/names.json" with { type: "json" };
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(
-  morgan("tiny", { stream: { write: (msg) => console.log(msg.trim()) } }),
-);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(morgan("tiny"));
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.render("index", { names });
 });
 
 app.get("/resume", (req, res) => {
