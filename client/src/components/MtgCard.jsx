6f1106a1ac47a6a761d457a4855d74dd509a41d0
Evan <timmonsevan@gmail.com>
2026-06-10 16:58:26 -0400

    ported name generator to react

--- client/src/components/MtgCard.jsx
+++ client/src/components/MtgCard.jsx
@@ -1,13 +1,13 @@
-import './MtgCard.css'
+import "./MtgCard.css";
 
 function MtgCard() {
   return (
     <div title="Random MTG Card" className="content-container">
       <h2>Random MTG Card</h2>
       <button className="btn-accent">Draw a card!</button>
-      <div className="mtg-card-display" />
+      <div className="mtg-card-display"></div>
     </div>
-  )
+  );
 }
 
-export default MtgCard
+export default MtgCard;
